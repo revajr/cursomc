ca8ca372d8a2309e51a3ed5f51882f40ab1155a9
revajr <revajr@gmail.com>
2018-02-01 22:48:13 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/revajr/cursomc/services/CategoriaService.java
+++ src/main/java/com/revajr/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.revajr.cursomc.dominio.Categoria;
 import com.revajr.cursomc.repositories.CategoriaRepository;
+import com.revajr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,12 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id: "+id
+					+ ", Tipo: "+Categoria.class.getName());
+		}
+			
 		return obj;
 	}
 }
