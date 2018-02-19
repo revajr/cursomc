59b82d83d76477ebc8b40f3e66f12849d06406a7
revajr <revajr@gmail.com>
2018-02-18 21:07:38 -0300

    Validação sintática com BeanValidation

--- src/main/java/com/revajr/cursomc/services/CategoriaService.java
+++ src/main/java/com/revajr/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.revajr.cursomc.dominio.Categoria;
+import com.revajr.cursomc.dto.CategoriaDTO;
 import com.revajr.cursomc.repositories.CategoriaRepository;
 import com.revajr.cursomc.services.exceptions.DataIntegrityException;
 import com.revajr.cursomc.services.exceptions.ObjectNotFoundException;
@@ -57,7 +58,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
-		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 }
