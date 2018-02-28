95f881eef98882286f1f8337fba014722c3acc75
revajr <revajr@gmail.com>
2018-02-28 00:47:51 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/revajr/cursomc/services/ClienteService.java
+++ src/main/java/com/revajr/cursomc/services/ClienteService.java
@@ -1,10 +1,19 @@
 package com.revajr.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.revajr.cursomc.dominio.Cliente;
+import com.revajr.cursomc.dominio.Cliente;
+import com.revajr.cursomc.dto.ClienteDTO;
 import com.revajr.cursomc.repositories.ClienteRepository;
+import com.revajr.cursomc.services.exceptions.DataIntegrityException;
 import com.revajr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,4 +32,39 @@ public class ClienteService {
 			
 		return obj;
 	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try{
+			repo.delete(id);
+		}catch (DataIntegrityViolationException dive) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas!");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null); 
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+	
+	
 }
