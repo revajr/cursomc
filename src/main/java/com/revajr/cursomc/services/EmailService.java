97b42adb7c918f3a158c397fe21a3e6acd9dfc3b
revajr <revajr@gmail.com>
2018-03-25 20:42:10 -0300

    Email HTML

--- src/main/java/com/revajr/cursomc/services/EmailService.java
+++ src/main/java/com/revajr/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.revajr.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.revajr.cursomc.dominio.Pedido;
@@ -10,5 +12,7 @@ public interface EmailService {
 	
 	void sendEmail(SimpleMailMessage msg);
 	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
+	void sendHtmlEmail(MimeMessage msg);
 }
