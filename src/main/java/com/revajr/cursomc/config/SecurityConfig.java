a1e3fbd4496ee03ffd92639741f338028d7237a3
revajr <revajr@gmail.com>
2018-04-09 23:24:23 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/revajr/cursomc/config/SecurityConfig.java
+++ src/main/java/com/revajr/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import com.revajr.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	@Autowired
@@ -41,11 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/produtos/**",
-			"/categorias/**",
-			"/clientes/**"
-			
+			"/categorias/**"			
 	};
 
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/clientes/**"			
+	};
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -59,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		// .csrf().disable() : para evitar ataque de armazenamento de sessão
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
