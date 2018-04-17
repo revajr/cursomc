3efc0b8cb874a2e052a60cdba278cfbd0191118d
revajr <revajr@gmail.com>
2018-04-16 23:57:21 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/revajr/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/revajr/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,17 @@
 package com.revajr.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.revajr.cursomc.dominio.Cliente;
 import com.revajr.cursomc.dominio.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/revajr/cursomc/services/PedidoService.java
+++ src/main/java/com/revajr/cursomc/services/PedidoService.java
@@ -1,12 +1,14 @@
 package com.revajr.cursomc.services;
 
-
-
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.revajr.cursomc.dominio.Cliente;
 import com.revajr.cursomc.dominio.ItemPedido;
 import com.revajr.cursomc.dominio.PagamentoComBoleto;
 import com.revajr.cursomc.dominio.Pedido;
@@ -16,6 +18,8 @@ import com.revajr.cursomc.repositories.ItemPedidoRepository;
 import com.revajr.cursomc.repositories.PagamentoRepository;
 import com.revajr.cursomc.repositories.PedidoRepository;
 import com.revajr.cursomc.repositories.ProdutoRepository;
+import com.revajr.cursomc.security.UserSS;
+import com.revajr.cursomc.services.exceptions.AuthorizationException;
 import com.revajr.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,52 +27,51 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EmailService emailService;
-	
+
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
-		
-		if(obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! id: "+id
-					+ ", Tipo: "+Pedido.class.getName());
+
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! id: " + id + ", Tipo: " + Pedido.class.getName());
 		}
-			
+
 		return obj;
 	}
-	
+
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
 		obj.setCliente(clienteRepository.findOne(obj.getCliente().getId()));
-		
+
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
-				
+
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-	
+
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		
+
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoRepository.findOne(ip.getProduto().getId()));
@@ -78,7 +81,16 @@ public class PedidoService {
 		itemPedidoRepository.save(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
-	
 	}
-	
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteRepository.findOne(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
+
 }
