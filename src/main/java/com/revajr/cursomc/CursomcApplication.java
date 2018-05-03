c27c9cb705ef8160f765465cfabe13038128ce25
revajr <revajr@gmail.com>
2018-05-03 19:03:59 -0300

    Enviando imagem via endpoint

--- src/main/java/com/revajr/cursomc/CursomcApplication.java
+++ src/main/java/com/revajr/cursomc/CursomcApplication.java
@@ -1,18 +1,12 @@
 package com.revajr.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.revajr.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
-	@Autowired
-	private S3Service s3service;
-	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -20,8 +14,5 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		
-			s3service.uploadFile("C:\\Users\\Revailton\\Documents\\Cursos\\UDEMY - Spring Boot com Ionic - Estudo de caso completo\\Joao.jpg");
-		
 	}
 }
